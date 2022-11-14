f3b5a3c2f3ccf0660448dd2ce0125ae9284ef10f
kadarkutir <35130946+kadarkutir@users.noreply.github.com>
2022-11-14 16:25:02 +0100

    initialize.sql added questions table creator

--- App/db/initialize.sql
+++ App/db/initialize.sql
@@ -4,3 +4,19 @@ create table if not EXISTS users (
     password text not null,
     email text not NULL
 );
+
+create table if not exists questions (
+    title text not null,
+    createdBy text not NULL,
+    question1 text not NULL,
+    question2 text not NULL,
+    question3 text not NULL,
+    question4 text not NULL,
+    question5 text not NULL,
+    question6 text not NULL,
+    question7 text not NULL,
+    question8 text not NULL,
+    question9 text not NULL,
+    question10 text not NULL,
+    createdAt datetime NOT NULL
+);
