7696499b9eb52c409de013c9b5eff126410057b5
kadarkutir <35130946+kadarkutir@users.noreply.github.com>
2022-11-21 16:30:23 +0100

    app added valasz adott ember adott kerdoivre

--- App/app.py
+++ App/app.py
@@ -195,5 +195,27 @@ def get_all_answers_on_questionnare(title):
 
     return flask.jsonify(answers)
 
+@app.route("/get_answers_by_user_and_title_my_questionnare/<title>/<user>")
+def get_answers_by_user_and_title_my_questionnare(title,user):
+
+    questions = db_con.get_questions_for_questionnare_by_title(con,title)
+    answers = db_con.get_answers_from_user_by_username_and_title(con,user,title)
+
+    result = [0 for i in range(0,(len(questions)+len(answers)))]
+    result[0] = questions[0]
+    result[1] = user
+
+    dif = 1
+    for i in range(2,len(result),2):
+        result[i] = questions[i-dif]
+        dif += 1
+
+    dif = 2
+    for j in range(3,len(result),2):
+        result[j] = answers[j-dif]
+        dif+=1
+    
+    return flask.jsonify(result)
+
 if __name__ == "__main__":
     app.run(debug=True,host="localhost",port=5000)
